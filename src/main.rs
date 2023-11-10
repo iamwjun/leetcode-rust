bc919fc897ec07aa12e5eebce4de6cf3bee830ce
iamwjun <cddxwujun@hotmail.com>
2023-11-10 11:38:17 +0800

    docs: 2540. 最小公共值

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 fn main() {
-    let s = "01000111";
-    // let s = "00111";
-    // let s = "111";
-    let (mut x, mut y, mut z) = (0, 0, 0);
-    let mut pre_char = 'a';
+    let nums1: Vec<i32> = vec![1, 1, 2];
+    let nums2: Vec<i32> = vec![2, 4];
 
-    for (i, c) in s.chars().enumerate() {
-        if c == '1' {
-            z += 1;
-            x = std::cmp::max(x, std::cmp::min(y, z) * 2);
-        } else if i == 0 || pre_char == '1' {
-            y = 1;
-            z = 0;
-        } else {
-            y += 1
+    let (mut left, mut right) = (0, 0);
+    let max_len = std::cmp::max(nums1.len(), nums2.len());
+    let mut res = -1;
+
+    while left < max_len {
+        match (nums1.get(left), nums2.get(right)) {
+            (Some(v1), Some(v2)) => {
+                if v1 == v2 {
+                    res = *v1;
+                    break;
+                } else if v1 < v2 {
+                    left += 1;
+                } else {
+                    right += 1;
+                }
+            },
+            (Some(_v1), None) => break,
+            (None, Some(_v2)) => break,
+            (None, None) => break
         }
-        pre_char = c;
     }
 
-    println!("{}", x)
+    println!("{:?}", res)
 }
