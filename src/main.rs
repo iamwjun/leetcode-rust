a50612e4d594b91f795df78dad62bda311a23b05
iamwjun <cddxwujun@hotmail.com>
2023-11-10 11:56:05 +0800

    docs: 2540. 最小公共值

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,18 @@ fn main() {
     let nums1: Vec<i32> = vec![1, 1, 2];
     let nums2: Vec<i32> = vec![2, 4];
 
+    let (len1, len2) = (nums1.len(), nums2.len());
     let (mut left, mut right) = (0, 0);
-    let max_len = std::cmp::max(nums1.len(), nums2.len());
-    let mut res = -1;
+    let mut res = nums1[left];
 
-    while left < max_len {
-        match (nums1.get(left), nums2.get(right)) {
-            (Some(v1), Some(v2)) => {
-                if v1 == v2 {
-                    res = *v1;
-                    break;
-                } else if v1 < v2 {
-                    left += 1;
-                } else {
-                    right += 1;
-                }
-            },
-            (Some(_v1), None) => break,
-            (None, Some(_v2)) => break,
-            (None, None) => break
+    while left < len1 &&  right < len2{
+        if nums1[left] == nums2[right] {
+            res = nums1[left]
+        }
+        if nums1[left] < nums2[right] {
+            left += 1
+        } else {
+            right += 1
         }
     }
 
