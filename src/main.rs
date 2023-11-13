585dc9086674b3459de2694448e4d5262ee4f923
iamwjun <cddxwujun@hotmail.com>
2023-11-13 16:04:35 +0800

    2300. 咒语和药水的成功对数

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,19 @@
 fn main() {
-    let spells: Vec<i32> = vec![5, 1, 3];
-    let potions: Vec<i32> = vec![1, 2, 3, 4, 5];
-    let success: i32 = 7;
-
-    println!("{:?}", successful_pairs(spells, potions, success))
-}
-
-fn search_index(potions: Vec<i32>, spell: i32, success: i32) -> i32 {
-    let mut size = potions.len() as i32;
-    let mut left = 0;
-    let mut right = size;
-    while left < right {
-        let mid = left + size / 2;
-        
-        if spell * potions[mid] >= success {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-
-        size = right - left;
-    }
-
-    0
-}
-
-fn successful_pairs(spells: Vec<i32>, mut potions: Vec<i32>, success: i32) -> Vec<i32> {
-    let len = potions.len();
-    let mut res: Vec<i32> = vec![];
+    let mut spells: Vec<i32> = vec![5, 1, 3];
+    let mut potions: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let success: i64 = 7;
 
     potions.sort_unstable();
-    
-    for s in spells {
-        println!("spells {s}");
-        let mut total = 0;
-        match potions.binary_search_by(|&x| {
-            if x * s  <= success {
-                std::cmp::Ordering::Equal
-            } else {
-                std::cmp::Ordering::Less
-            }
-        }) {
-            Ok(index) => {
-                println!("element is {}, s is {}, success is {}", potions[index], s, success);
-                total = len - index + 1
-            },
-            Err(_) => {
-                println!("no found");
-            },
+    let len = potions.len() as i32;
+
+    for s in spells.iter_mut() {
+        let idx = potions.partition_point(|&x| (x as i64 * *s as i64) < success) as i32;
+        if idx == len {
+            *s = 0;
+        } else {
+            *s = len - idx;
         }
-        res.push(total.try_into().unwrap())
     }
-    res
+
+    println!("{:?}", spells);
 }
