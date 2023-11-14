e72656d02135c6bacb22d3d2cf9d31e6af3dbf22
iamwjun <cddxwujun@hotmail.com>
2023-11-14 11:48:56 +0800

    docs: 26. 删除有序数组中的重复项

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let mut nums = vec![3,2,2,3];
-    let val = 3;
 
-    println!("{:?}", remove_element(&mut nums, val));
+    println!("{:?}", remove_element(&mut nums));
 
 }
 
-fn remove_element(nums: &mut Vec<i32>, val: i32) -> usize {
-    nums.retain(|&x| x != val);
+fn remove_element(nums: &mut Vec<i32>) -> usize {
+    let mut unique_set = std::collections::HashSet::new();
+    nums.retain(|&x| unique_set.insert(x));
     nums.len()
 }
