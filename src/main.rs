02c05faeb31260f1610999d452db7cb6b3a8b690
iamwjun <cddxwujun@hotmail.com>
2023-11-14 11:38:42 +0800

    docs: 27. 移除元素

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 fn main() {
-    let mut nums1: Vec<i32> = vec![1, 2, 3, 0, 0, 0];
-    let m = 3;
-    let mut nums2: Vec<i32> = vec![2, 5, 6];
-    let n = 3;
+    let mut nums = vec![3,2,2,3];
+    let val = 3;
 
-    println!("{:?}", merge(&mut nums1, m, &mut nums2, n));
+    println!("{:?}", remove_element(&mut nums, val));
 
 }
 
-fn merge<'a>(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) -> Vec<i32> {
-    for i in 0..n {
-        nums1[m as usize + i as usize] = nums2[i as usize]
-    }
-    nums1.sort();
-
-    nums1.to_vec()
+fn remove_element(nums: &mut Vec<i32>, val: i32) -> usize {
+    nums.retain(|&x| x != val);
+    nums.len()
 }
