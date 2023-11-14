ed378982a71acd03779707f0449704610461da3d
iamwjun <cddxwujun@hotmail.com>
2023-11-14 09:25:23 +0800

    docs: 88. 合并两个有序数组

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,18 @@
-struct NumArray {
-    nums: Vec<i32>
-}
-
-
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl NumArray {
-
-    fn new(nums: Vec<i32>) -> Self {
-        NumArray { nums }
-    }
-    
-    fn update(&mut self, index: i32, val: i32) {
-        if (index as usize) < self.nums.len() {
-            // 更新数组中的元素
-            self.nums[index as usize] = val;
-        }
-    }
-    
-    fn sum_range(&self, left: i32, right: i32) -> i32 {
-        self.nums[(left as usize)..=(right as usize)].iter().sum()
-    }
-}
-
 fn main() {
-    let mut num_array = NumArray::new(vec![1, 2, 3, 4, 5]);
+    let mut nums1: Vec<i32> = vec![1, 2, 3, 0, 0, 0];
+    let m = 3;
+    let mut nums2: Vec<i32> = vec![2, 5, 6];
+    let n = 3;
 
-    println!("Original Array: {:?}", num_array.nums);
+    println!("{:?}", merge(&mut nums1, m, &mut nums2, n));
 
-    num_array.update(2, 10);
+}
 
-    println!("Updated Array: {:?}", num_array.nums);
+fn merge<'a>(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) -> Vec<i32> {
+    for i in 0..n {
+        nums1[m as usize + i as usize] = nums2[i as usize]
+    }
+    nums1.sort();
 
-    let sum = num_array.sum_range(1, 3);
-    println!("Sum of range: {}", sum);
+    nums1.to_vec()
 }
