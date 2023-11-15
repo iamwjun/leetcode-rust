4bfe798f55a662adc25ebd59d7fe16e37cebbbdc
iamwjun <cddxwujun@hotmail.com>
2023-11-15 12:01:06 +0800

    docs: 169. 多数元素 解法二

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,7 @@ fn main() {
     println!("{:?}", majority_element(nums));
 }
 
-fn majority_element(nums: Vec<i32>) -> i32 {
-    let mut counts = std::collections::HashMap::new();
-
-    for n in nums {
-        *counts.entry(n).or_insert(0) += 1;
-    }
-
-    let mut max_element: Option<i32> = None;
-    let mut max_count = 0;
-
-    for (&element, &count) in counts.iter() {
-        if count > max_count {
-            max_count = count;
-            max_element = Some(element);
-        }
-    }
-
-    max_element.unwrap()
+fn majority_element(mut nums: Vec<i32>) -> i32 {
+    nums.sort();
+    nums[nums.len() / 2]
 }
