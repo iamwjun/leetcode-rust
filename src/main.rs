56445984cb139677e3a6a54759b5a54ad9af92ab
iamwjun <cddxwujun@hotmail.com>
2023-11-17 11:51:06 +0800

    docs: 55. 跳跃游戏

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 fn main() {
-    let prices = vec![2,3,1,1,4];
+    let prices = vec![3,2,1,0,4];
 
     println!("{:?}", can_jump(prices));
     
 }
 
 fn can_jump(nums: Vec<i32>) -> bool {
-    let mut curr = 0;
+    let len = nums.len();
+    let mut max_reach = 0;
 
-    while curr < nums.len() {
-        curr += nums[curr] as usize
+    for (i, v) in nums.iter().enumerate() {
+        if i > max_reach {
+            return  false;
+        }
+
+        max_reach = max_reach.max(i + (*v as usize));
+
+        if max_reach >= len - 1 {
+            break;
+        }
     }
-
-    curr == nums.len()
+    true
 }
