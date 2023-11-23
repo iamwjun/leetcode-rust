454428964e256bf0f1658ef7d1f731c7e386d5cd
iamwjun <cddxwujun@hotmail.com>
2023-11-23 17:47:36 +0800

    docs: 167. 两数之和 II 解法三

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ fn main() {
 }
 
 fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-    for (i, num) in numbers.iter().enumerate() {
-        let value = target - num;
-        if let Some(index) = numbers.iter().rev().position(|&x| x == value) {
-            return vec![i as i32 + 1, index as i32 + 1];
+    let (mut a, mut b) = (0, numbers.len() - 1);
+
+    loop {
+        match target.cmp(&(numbers[a] + numbers[b])) {
+            std::cmp::Ordering::Equal => return vec![a as i32 + 1, b as i32 +1],
+            std::cmp::Ordering::Greater => a += 1,
+            std::cmp::Ordering::Less => b -= 1
         }
     }
-    vec![]
 }
