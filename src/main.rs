d62d7f882a53a001a7c37fe89ff2d9e02dcea267
iamwjun <cddxwujun@hotmail.com>
2023-11-23 16:57:13 +0800

    docs: 392. 判断子序列

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
-    let s = String::from("0P");
+    let s = String::from("abc");
+    let t = String::from("ahbgadc");
 
-    println!("{:?}", is_palindrome(s));
+    println!("{:?}", is_subsequence(s, t));
 }
 
-fn is_palindrome(s: String) -> bool {
-    let s = s.chars().filter(|&c| c.is_alphanumeric()).collect::<String>().to_lowercase();
-    let reversed = s.chars().rev().collect::<String>();
-    s == reversed
+fn is_subsequence(s: String, t: String) -> bool {
+    let mut s_iter = s.chars().peekable();
+    println!("{:?}", s_iter);
+    
+    for char_t in t.chars() {
+        println!("t for {}", char_t);
+        if let Some(&chart_s) = s_iter.peek() {
+            println!("{}", &chart_s);
+            if char_t == chart_s {
+                s_iter.next();
+            }
+        }
+    }
+    println!("{:?}", s_iter);
+    s_iter.peek().is_none()
 }
