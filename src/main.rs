7fed7a10343e23312844115785e9a5e37bcc1600
iamwjun <cddxwujun@hotmail.com>
2023-11-28 13:59:12 +0800

    docs: 128. 最长连续序列

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,33 @@
-use std::collections::VecDeque;
-
-struct FrontMiddleBackQueue {
-    deque: VecDeque<i32>
-}
-
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
-impl FrontMiddleBackQueue {
-
-    fn new() -> Self {
-        FrontMiddleBackQueue { deque: VecDeque::new() }
-    }
-    
-    fn push_front(&mut self, val: i32) {
-        self.deque.insert(0, val);
-    }
-    
-    fn push_middle(&mut self, val: i32) {
-        let pos = self.len() / 2;
-        self.deque.insert(pos, val);
-    }
-    
-    fn push_back(&mut self, val: i32) {
-        self.deque.insert(self.len(), val)
-    }
-    
-    fn pop_front(&mut self) -> i32 {
-        if let Some(n) = self.deque.pop_front() {
-            return  n;
-        }
-        -1
-    }
-    
-    fn pop_middle(&mut self) -> i32 {
-        if let Some(n) = self.deque.remove(self.middle_index()) {
-            return n;
-        }
-        -1
-    }
-    
-    fn pop_back(&mut self) -> i32 {
-        if let Some(n) = self.deque.pop_back() {
-            return n;
-        }
-        -1
-    }
-
-    fn middle_index(&self) -> usize {
-        (self.len() - 1) / 2
-    }
-
-    fn len(&self) -> usize {
-        self.deque.len()
-    }
-}
-
-/**
- * Your FrontMiddleBackQueue object will be instantiated and called as such:
- * let obj = FrontMiddleBackQueue::new();
- * obj.push_front(val);
- * obj.push_middle(val);
- * obj.push_back(val);
- * let ret_4: i32 = obj.pop_front();
- * let ret_5: i32 = obj.pop_middle();
- * let ret_6: i32 = obj.pop_back();
- */
+use std::collections::HashSet;
 
 fn main() {
-    let mut q = FrontMiddleBackQueue::new();
+    let nums: Vec<i32> = vec![9,1,4,7,3,-1,0,5,8,-1,6];
 
-    println!("{:?}", q.push_front(1));
+    println!("{:?}", longest_consecutive(nums));
+}
 
-    println!("{:?}", q.push_back(2));
+fn longest_consecutive(nums: Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
 
-    println!("{:?}", q.push_middle(3));
+    let nums_set: HashSet<i32> = nums.into_iter().collect();
+    let mut longest = 0;
 
-    println!("{:?}", q.push_middle(4));
+    for &num in nums_set.iter() {
+        if !nums_set.contains(&(num - 1)) {
+            let mut curr_num = num;
+            let mut curr_length = 1;
 
-    println!("{:?}", q.deque);
+            while nums_set.contains(&(curr_num + 1)) {
+                curr_num += 1;
+                curr_length += 1;
+            }
 
-    println!("{:?}", q.pop_front());
+            longest = longest.max(curr_length);
+        }
+    }
 
-    println!("{:?}", q.deque);
-
-    println!("{:?}", q.pop_middle());
-
-    println!("{:?}", q.deque);
-
-    println!("{:?}", q.pop_middle());
-
-    println!("{:?}", q.deque);
-
-    println!("{:?}", q.pop_back());
-
-    println!("{:?}", q.deque);
-
-    println!("{:?}", q.pop_front());
+    longest
 }
 
