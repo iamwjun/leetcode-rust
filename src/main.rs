58a137d0ff00737f219a411d5cd9dc416a04fbb1
iamwjun <cddxwujun@hotmail.com>
2023-11-28 15:33:55 +0800

    docs: 56. 合并区间 解法三

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
 fn main() {
-    let intervals: Vec<Vec<i32>> = vec![vec![2,3],vec![4,5],vec![6,7],vec![8,9],vec![1,10]];
+    let intervals: Vec<Vec<i32>> = vec![vec![1,6], vec![8,10], vec![15,18]];
 
     println!("{:?}", merge(intervals));
 }
 
-fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    if intervals.is_empty() {
-        return vec![];
-    }
-    let mut sort_intervals = intervals;
-    sort_intervals.sort_unstable_by_key(|a| a[0]);
+fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    intervals.sort_unstable_by_key(|a| a[0]);
 
     let mut vec: Vec<Vec<i32>> = vec![];
-    let mut curr_intervals = sort_intervals[0].clone();
+    vec.push(intervals.first().unwrap().clone());
 
-    for interval in sort_intervals.iter().skip(1) {
-        if interval[0] <= curr_intervals[1] {
-            curr_intervals[1] = curr_intervals[1].max(interval[1]);
+    for interval in intervals.iter().skip(1) {
+        if interval[0] <= vec.last().unwrap()[1] {
+            vec.last_mut().unwrap()[1] = vec.last().unwrap()[1].max(interval[1]);
         } else {
-            vec.push(curr_intervals);
-            curr_intervals = interval.to_vec();
+            vec.push(interval.clone());
         }
     }
 
-    vec.push(curr_intervals);
-    
     vec
 }
 
