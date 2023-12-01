58876d8c87b2a9092cdfed659b8298ffafb51d07
iamwjun <cddxwujun@hotmail.com>
2023-12-01 11:43:47 +0800

    docs: 2661. 找出叠涂元素 解法二

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,37 @@
 use std::collections::HashMap;
 
 fn main() {
-    let arr = vec![6,2,3,1,4,5];
-    let mat: Vec<Vec<i32>> = vec![vec![5,1], vec![2,4], vec![6,3]];
+    let arr = vec![1,4,5,2,6,3];
+    let mat: Vec<Vec<i32>> = vec![vec![4,3,5], vec![1,2,6]];
 
     println!("{:?}", first_complete_index(arr, mat));
 }
 
 fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
-    let mut coordinates: HashMap<i32, Vec<usize>> = HashMap::new();
-    let mut rows: HashMap<usize, Vec<i32>> = HashMap::new();
-    let mut columns: HashMap<usize, Vec<i32>> = HashMap::new();
+    let mut coordinates: HashMap<i32, (usize, usize)> = HashMap::new();
+    let row_len = mat.len();
+    let column_len = mat[0].len();
 
-    for (row, m) in mat.iter().enumerate() {
-        rows.entry(row).or_insert(m.to_vec());
-        for (column, &v) in m.iter().enumerate() {
-            coordinates.insert(v, vec![row, column]);
-            columns.entry(column).or_insert(Vec::new()).push(v)
+    for i in 0..row_len {
+        for j in 0..column_len {
+            coordinates.insert(mat[i][j], (i, j));
         }
     }
 
+    let mut rows_count = vec![0;row_len];
+    let mut columns_count = vec![0;column_len];
 
-    for (i, a) in arr.iter().enumerate() {
-        if let Some(v) = coordinates.get(a) {
-            let (row, column) = (v[0], v[1]);
-
-            if let Some(curr_row) = rows.get_mut(&row) {
-                curr_row[column] = 0;
-                if curr_row.iter().all(|&x| x == 0){
-                    return i as i32;
-                }
-            }
-
-            if let Some(curr_column) = columns.get_mut(&column) {
-                curr_column[row] = 0;
-                if curr_column.iter().all(|&x| x == 0){
-                    return i as i32;
-                }
+    for (index,a) in arr.iter().enumerate() {
+        
+        if let Some(&(i, j)) = coordinates.get(a) {
+            rows_count[i] += 1;
+            columns_count[j] += 1;
+            if rows_count[i] == column_len || columns_count[j] == row_len {
+                return index as i32;
             }
         }
     }
 
-    -1
+    unreachable!()
 }
 
