c0957a238960d344b141a06aecf8cf46dee42506
iamwjun <cddxwujun@hotmail.com>
2023-12-01 14:39:00 +0800

    20. 有效的括号

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
-use std::collections::HashMap;
-
 fn main() {
-    let arr = vec![1,4,5,2,6,3];
-    let mat: Vec<Vec<i32>> = vec![vec![4,3,5], vec![1,2,6]];
+    let s = String::from("()[]{}");
 
-    println!("{:?}", first_complete_index(arr, mat));
+    println!("{:?}", is_valid(s));
+
+    println!("{}", 1 << 0);
+
+    println!("{}", 1 << 1);
 }
 
-fn first_complete_index(arr: Vec<i32>, mat: Vec<Vec<i32>>) -> i32 {
-    let mut coordinates: HashMap<i32, (usize, usize)> = HashMap::new();
-    let row_len = mat.len();
-    let column_len = mat[0].len();
+fn is_valid(s: String) -> bool {
+    let mut v: Vec<u8> = vec![];
 
-    for i in 0..row_len {
-        for j in 0..column_len {
-            coordinates.insert(mat[i][j], (i, j));
+    for char in s.into_bytes() {
+        match char {
+            b')' | b']' | b'}' => if Some(char) != v.pop() {
+                return false;
+            },
+            _ => v.push(char + (1 << (char & 1)))
         }
     }
-
-    let mut rows_count = vec![0;row_len];
-    let mut columns_count = vec![0;column_len];
-
-    for (index,a) in arr.iter().enumerate() {
-        
-        if let Some(&(i, j)) = coordinates.get(a) {
-            rows_count[i] += 1;
-            columns_count[j] += 1;
-            if rows_count[i] == column_len || columns_count[j] == row_len {
-                return index as i32;
-            }
-        }
-    }
-
-    unreachable!()
+    v.is_empty()
 }
 
