5fa81c356d91f960eb83ebdd5067836c3cbd5acf
iamwjun <cddxwujun@hotmail.com>
2023-12-21 13:53:07 +0800

    docs: 寻找峰值

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 fn main() {
     let mut s = String::from("abcd");
 
-    println!("{}", make_smallest_palindrome(s));
+    // println!("{}", make_smallest_palindrome(s));
+
+    let nums = vec![1];
+
+    println!("{}", find_peak_element(nums));
+}
+
+fn find_peak_element(nums: Vec<i32>) -> i32 {
+    if nums.len() < 3 {
+        return *nums.iter().max().unwrap();
+    }
+    for i in 1..nums.len() - 1 {
+        if nums[i] > nums[i - 1] && nums[i] > nums[i+1] {
+            return i as i32;
+        }
+    }
+    -1
 }
 
 fn make_smallest_palindrome(s: String) -> String {
