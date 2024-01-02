c5995544a43b608de2134b57ff992bc1c8562395
iamwjun <cddxwujun@hotmail.com>
2024-01-02 17:34:11 +0800

    docs: 151. 反转字符串中的单词

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,11 @@
 fn main() {
-    let mut s = String::from("abcd");
+    let s = String::from("  hello world  ");
 
-    // println!("{}", make_smallest_palindrome(s));
-
-    let nums = vec![1];
-
-    println!("{}", find_peak_element(nums));
+    println!("{}", reverse_words(s));
 }
 
-fn find_peak_element(nums: Vec<i32>) -> i32 {
-    if nums.len() < 3 {
-        return *nums.iter().max().unwrap();
-    }
-    for i in 1..nums.len() - 1 {
-        if nums[i] > nums[i - 1] && nums[i] > nums[i+1] {
-            return i as i32;
-        }
-    }
-    -1
+fn reverse_words(s: String) -> String {
+    let words: Vec<&str> = s.split_whitespace().rev().into_iter().collect();
+
+    words.join(" ")
 }
-
-fn make_smallest_palindrome(s: String) -> String {
-    let s_chars: Vec<char> = s.chars().collect();
-    println!("{:?}", s_chars);
-    
-    let mut t_chars = Vec::new();
-    for char in s_chars {
-        t_chars.push(char);
-        t_chars.push('#');
-    }
-
-    println!("{:?}", t_chars);
-    s
-}
-
-
