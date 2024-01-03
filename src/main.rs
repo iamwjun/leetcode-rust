fd7548f27eeead96cbff91e753bcf881c51027e9
iamwjun <cddxwujun@hotmail.com>
2024-01-03 14:24:49 +0800

    docs: 238. 除自身以外数组的乘积 解法三

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 fn main() {
-    let nums = vec![1, 2, 3, 4];
+    let nums = vec![-1,1,0,-3,3];
 
     println!("{:?}", product_except_self(nums));
 }
 
-fn product_except_self(mut nums: Vec<i32>) -> Vec<i32> {
-    let product = nums.iter().fold(1, |acc, e| acc * e);
+fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+    let mut result: Vec<i32> = Vec::new();
+    let len = nums.len();
 
-    for num in &mut nums {
-        *num = product / *num;
+    let mut left_product = 1;
+    let mut left_products = vec![1;len];
+    let mut right_product = 1;
+    let mut right_products = vec![1;len];
+    
+
+    for i in 1..len {
+        left_product *= nums[i - 1];
+        left_products[i] = left_product;
+
+        right_product *= nums[len - i];
+        right_products[len - i - 1] = right_product;
     }
 
-    nums
+    for i in 0..len {
+        result.push(left_products[i] * right_products[i]);
+    }
+
+    result
 }
