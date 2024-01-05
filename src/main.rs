030bae50afb4864c165e805194fd107a09d39ca9
iamwjun <cddxwujun@hotmail.com>
2024-01-05 18:08:48 +0800

    docs: 1944. 队列中可以看到的人数

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,34 @@
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-
-    fn insert(&mut self, val: i32) {
-        let node = Box::new(ListNode {
-            val,
-            next: self.next.take(),
-        });
-        self.next = Some(node);
-    }
-
-    fn pring(&self) {
-        let mut current = Some(self);
-        while let Some(node) = current {
-            println!("val is {}", node.val);
-            current = node.next.as_ref().map(|n| &**n);
-        }
-        println!("None")
-    }
-}
-
 fn main() {
-    // 手动赋值
-    // let mut head = ListNode::new(4);
-    // head.next = Some(Box::new(ListNode::new(5)));
-    // head.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
-    // head.next.as_mut().unwrap().next.as_mut().unwrap().next = Some(Box::new(ListNode::new(10)));
+    let heights = vec![5,1,2,3,10];
 
-    // head.pring();
-
-    let vec = vec![5, 2, 13, 3, 8];
-    let head = create_list_node(vec);
-
-    println!("head is {:?}", head);
-
-    println!("{:?}", remove_nodes(head));
+    println!("{:?}", can_see_persons_count(heights));
 }
 
-fn remove_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    match head {
-        Some(mut curr) => {
-            if let Some(next) = curr.next.take() {
-                let prev = remove_nodes(Some(next));
-                if curr.val < prev.as_ref().unwrap().val {
-                    prev
-                } else {
-                    curr.next = prev;
-                    Some(curr)
-                }
-            } else {
-                Some(curr)
+fn can_see_persons_count(heights: Vec<i32>) -> Vec<i32> {
+    let len = heights.len();
+    let mut ret = vec![0;len];
+
+    for i in 0..len {
+        let curr = heights[i];
+        let mut max_height = std::i32::MIN;
+        let mut is_greater = false;
+        
+        for j in i+1..len {
+            if heights[j] < curr && !is_greater && heights[j] > max_height {
+                ret[i] += 1;
             }
-        },
-        None => None,
-    }
-}
 
-fn create_list_node(vec: Vec<i32>) -> Option<Box<ListNode>> {
-    let mut ret = None;
-    let mut pre_node = &mut ret;
+            if is_greater || j > i + 2 {
+                max_height = max_height.max(heights[j]);
+            }
 
-    for v in vec.iter() {
-        let node = Some(Box::new(ListNode::new(*v)));
-        *pre_node = node;
-
-        match pre_node {
-            Some(n) => { pre_node = &mut n.next },
-            None => { unreachable!(); },
+            if heights[j] >= curr {
+                is_greater = true;
+                
+                if heights[j] > max_height {
+                    ret[i] += 1;
+                }
+            }
         }
     }
 
