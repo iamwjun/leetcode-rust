e5b1ab3b92c58e8a918a92ad78aaf7e1781615db
iamwjun <cddxwujun@hotmail.com>
2024-01-08 13:53:00 +0800

    docs: 1944. 队列中可以看到的人数 单调栈

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,19 @@ fn main() {
 
 fn can_see_persons_count(heights: Vec<i32>) -> Vec<i32> {
     let len = heights.len();
-    let mut ret = vec![0; len];
+    let mut ret: Vec<i32> = vec![0; len];
+    let mut stack: Vec<i32> = Vec::new();
 
-    for i in 0..len {
-        let mut max_height = std::i32::MIN;
-        let mut greater_curr = false;
-        let mut is_greater = false;
-
-        for j in i + 1..len {
-            if heights[j] < heights[i] {
-                if heights[j] > max_height && (!is_greater || !greater_curr) {
-                    ret[i] += 1;
-                }
-            } else {
-                if heights[j] > max_height {
-                    ret[i] += 1;
-                }
-                break;
-            }
-
-            if max_height >= heights[j] && !is_greater {
-                is_greater = true;
-            }
-
-            max_height = max_height.max(heights[j]);
-
-            if max_height >= heights[i] && !greater_curr {
-                greater_curr = true;
-            }
+    for (i, &h) in heights.iter().enumerate().rev() {
+        while !stack.is_empty() && *stack.last().unwrap() < h {
+            stack.pop();
+            ret[i] += 1;
         }
+        if !stack.is_empty() {
+            ret[i] += 1;
+        }
+
+        stack.push(h);
     }
 
     ret
