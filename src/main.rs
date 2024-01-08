9a78eeb10129013e3de693a4ae9127c54bf1825b
iamwjun <cddxwujun@hotmail.com>
2024-01-08 10:39:48 +0800

    docs: 1944. 队列中可以看到的人数

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 fn main() {
-    let heights = vec![5,1,2,3,10];
+    let heights = vec![11, 19, 12, 15, 14, 18, 7, 1, 8, 9];
 
     println!("{:?}", can_see_persons_count(heights));
 }
 
 fn can_see_persons_count(heights: Vec<i32>) -> Vec<i32> {
     let len = heights.len();
-    let mut ret = vec![0;len];
+    let mut ret = vec![0; len];
 
     for i in 0..len {
-        let curr = heights[i];
         let mut max_height = std::i32::MIN;
+        let mut greater_curr = false;
         let mut is_greater = false;
-        
-        for j in i+1..len {
-            if heights[j] < curr && !is_greater && heights[j] > max_height {
-                ret[i] += 1;
-            }
 
-            if is_greater || j > i + 2 {
-                max_height = max_height.max(heights[j]);
-            }
-
-            if heights[j] >= curr {
-                is_greater = true;
-                
+        for j in i + 1..len {
+            if heights[j] < heights[i] {
+                if heights[j] > max_height && (!is_greater || !greater_curr) {
+                    ret[i] += 1;
+                }
+            } else {
                 if heights[j] > max_height {
                     ret[i] += 1;
                 }
+                break;
+            }
+
+            if max_height >= heights[j] && !is_greater {
+                is_greater = true;
+            }
+
+            max_height = max_height.max(heights[j]);
+
+            if max_height >= heights[i] && !greater_curr {
+                greater_curr = true;
             }
         }
     }
