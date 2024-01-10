2c96e5bffd74f35db19ebc5fdebb191afced5e16
iamwjun <cddxwujun@hotmail.com>
2024-01-10 11:09:47 +0800

    2707. 字符串中的额外字符

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 fn main() {
-    let points: Vec<Vec<i32>> = vec![vec![0,0],vec![1,0],vec![-1,0],vec![0,1],vec![0,-1]];
+    let s = String::from("leetscode");
+    let dictionary: Vec<String> = vec!["leet".to_string(),"code".to_string(),"leetcode".to_string()];
 
-    println!("{:?}", number_of_boomerangs(points));
+    println!("{:?}", min_extra_char(s, dictionary));
 }
 
-fn squared_euclidean_distance(point1: Vec<i32>, point2: Vec<i32>) -> f64 {
-    ((point2[0] - point1[0]).pow(2) + (point2[1] - point1[1]).pow(2)) as f64
-}
-
-fn number_of_boomerangs(points: Vec<Vec<i32>>) -> i32 {
-    let mut ret: i32 = 0;
-    
-    for i in 1..points.len() - 1 {
-        let point1 = points[i-1].clone();
-        let point2 = points[i].clone();
-        let point3 = points[i].clone();
-        let point4 = points[i+1].clone();
-
-        if squared_euclidean_distance(point1, point2) == squared_euclidean_distance(point3, point4) {
-            ret += 2
-        }
-    }
-
-    ret
+fn min_extra_char(s: String, dictionary: Vec<String>) -> i32 {
+    -1
 }
