0203eb2622454ac2a49058489e055a8c3bbe3f27
iamwjun <cddxwujun@hotmail.com>
2024-01-12 10:00:29 +0800

    docs: 2085. 统计出现过一次的公共字符串

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-fn main() {
-    let s = String::from("ABFCACDB");
+use std::collections::HashMap;
 
-    println!("{:?}", min_length(s));
+fn main() {
+    let words1: Vec<String> = vec![
+        "leetcode".to_string(),
+        "is".to_string(),
+        "amazing".to_string(),
+        "as".to_string(),
+        "is".to_string()
+    ];
+    let words2: Vec<String> = vec![
+        "amazing".to_string(),
+        "leetcode".to_string(),
+        "is".to_string()
+    ];
+
+    println!("{:?}", count_words(words1, words2));
 }
 
-fn min_length(s: String) -> usize {
-    let ab = "AB".as_bytes();
-    let cd = "CD".as_bytes();
-    s.as_bytes()
-        .windows(2)
-        .filter(|&window| window == ab)
-        .count()
-        + s.as_bytes()
-            .windows(2)
-            .filter(|&window| window == cd)
-            .count()
+fn count_words(words1: Vec<String>, words2: Vec<String>) -> i32 {
+    let mut map: HashMap<String, u32> = HashMap::new();
+
+    for word in words1 {
+        map.entry(word)
+            .and_modify(|value| { *value += 2 })
+            .or_insert(1);
+    }
+
+    for word in words2 {
+        map.entry(word).and_modify(|value| { *value += 1 });
+    }
+
+    map.values().filter(|&&value| value == 2).count() as i32
 }
