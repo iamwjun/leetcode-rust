1862f2ae84ed194f875b44f5f2d4f4930de49135
iamwjun <cddxwujun@hotmail.com>
2024-01-24 18:07:01 +0800

    docs: 2865. 美丽塔 I

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 fn main() {
-    let nums = vec![25, 9, 7];
+    let max_heights = vec![5, 3, 4, 1, 1];
 
-    println!("alternating_subarray {:?}", alternating_subarray(nums));
+    println!("{:?}", maximum_sum_of_heights(max_heights));
 }
 
-fn alternating_subarray(nums: Vec<i32>) -> i32 {
-    let mut ret = -1;
+fn maximum_sum_of_heights(max_heights: Vec<i32>) -> i64 {
+    let ret = -1;
 
-    for (i, m) in nums.iter().enumerate() {
-        for (j, n) in nums.iter().skip(i + 1).enumerate() {
-            let length = j + 2;
-            if n - m == (length - 1) as i32 % 2 {
-                ret = ret.max(length as i32);
-            } else {
-                break;
-            }
+    if let Some((index, max_value)) = max_heights
+        .iter()
+        .enumerate()
+        .max_by_key(|&(_, &value)| value)
+    {
+        for i in 0..index {
+            println!("left {}", i);
+        }
+
+        for j in index + 1..max_heights.len() {
+            println!("right {}", j);
         }
     }
 
