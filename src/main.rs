07a633941cb6e1199ab829fe97b6f13fa3e22d34
iamwjun <cddxwujun@hotmail.com>
2024-01-25 11:04:56 +0800

    docs: 2859. 计算 K 置位下标对应元素的和

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
 fn main() {
-    let max_heights: Vec<i32> = vec![5,2,4,4];
+    let nums: Vec<i32> = vec![5, 10, 1, 5, 2];
+    let k = 1;
 
-    println!("{:?}", maximum_sum_of_heights(max_heights));
+    println!("{:?}", sum_indices_with_k_set_bits(nums, k));
 }
 
-fn maximum_sum_of_heights(max_heights: Vec<i32>) -> i64 {
-    let mut maxinum: i64 = 0;
-    let len = max_heights.len();
-
-    for index in 0..len {
-        let mut ret: i64 = max_heights[index] as i64;
-        let mut pre = max_heights[index];
-
-        for i in (0..index).rev() {
-            if max_heights[i] > pre  {
-                ret += pre as i64;
-            } else {
-                ret += max_heights[i] as i64;
-                pre = max_heights[i];
-            }
-        }
-
-        pre = max_heights[index];
-        for j in index + 1..len {
-            if max_heights[j] > pre  {
-                ret += pre as i64;
-            } else {
-                ret += max_heights[j] as i64;
-                pre = max_heights[j];
-            }
-        }
-
-        maxinum = maxinum.max(ret);
-    }
-
-
-    maxinum
+fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
+    nums.iter().enumerate().fold(0, |acc, (i, &num)| {
+        acc + if i.count_ones() == k as u32 { num } else { 0 }
+    })
 }
