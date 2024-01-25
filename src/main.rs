c399dad9d9a919830b26bc9713f74e8e6e5fce51
iamwjun <cddxwujun@hotmail.com>
2024-01-25 17:54:32 +0800

    docs: 6. Z 字形变换

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,54 @@
 fn main() {
-    let nums: Vec<i32> = vec![5, 10, 1, 5, 2];
-    let k = 1;
+    let s = String::from("A");
+    let num_rows = 3;
 
-    println!("{:?}", sum_indices_with_k_set_bits(nums, k));
+    println!("{:?}", convert(s, num_rows));
 }
 
-fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
-    nums.iter().enumerate().fold(0, |acc, (i, &num)| {
-        acc + if i.count_ones() == k as u32 { num } else { 0 }
-    })
+fn convert(s: String, num_rows: i32) -> String {
+    if num_rows == 1 || s.len() == 1 {
+        return s;
+    }
+    let mut matrix: Vec<Vec<char>> = Vec::new();
+    let chars: Vec<char> = s.chars().collect();
+    let mut ret: Vec<char> = Vec::new();
+    let rows = num_rows as usize;
+    let num_columns = s.len();
+
+    for _i in 0..rows {
+        matrix.push(vec!['0'; num_columns]);
+    }
+
+    let mut start = 0;
+    for j in 0..num_columns {
+        let n = j % (rows - 1);
+
+        if start >= num_columns {
+            break;
+        }
+
+        if n == 0 {
+            for i in 0..rows {
+                if let Some(char) = chars.get(start) {
+                    matrix[i][j] = *char;
+                    start += 1;
+                }
+            }
+        } else {
+            if let Some(char) = chars.get(start) {
+                matrix[rows - n - 1][j] = *char;
+                start += 1;
+            }
+        }
+    }
+
+    for r in 0..rows {
+        for c in 0..num_columns {
+            if matrix[r][c] != '0' {
+                ret.push(matrix[r][c]);
+            }
+        }
+    }
+
+    ret.iter().collect::<String>()
 }
