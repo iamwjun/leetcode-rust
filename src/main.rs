581f2873fe6a094006cd30e1c7b51aa20cde86d3
iamwjun <cddxwujun@hotmail.com>
2024-01-29 17:36:49 +0800

    docs: 514. 自由之路

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,22 @@
 fn main() {
-    let s = String::from("A");
-    let num_rows = 3;
+    let ring = String::from("godding");
+    let key = String::from("godding");
 
-    println!("{:?}", convert(s, num_rows));
+    println!("{:?}", find_rotate_steps(ring, key));
 }
 
-fn convert(s: String, num_rows: i32) -> String {
-    if num_rows == 1 || s.len() == 1 {
-        return s;
-    }
-    let mut matrix: Vec<Vec<char>> = Vec::new();
-    let chars: Vec<char> = s.chars().collect();
-    let mut ret: Vec<char> = Vec::new();
-    let rows = num_rows as usize;
-    let num_columns = s.len();
+fn find_rotate_steps(ring: String, key: String) -> i32 {
+    let mut ret = 0;
+    let mut chars: Vec<char> = ring.chars().collect();
 
-    for _i in 0..rows {
-        matrix.push(vec!['0'; num_columns]);
-    }
-
-    let mut start = 0;
-    for j in 0..num_columns {
-        let n = j % (rows - 1);
-
-        if start >= num_columns {
-            break;
-        }
-
-        if n == 0 {
-            for i in 0..rows {
-                if let Some(char) = chars.get(start) {
-                    matrix[i][j] = *char;
-                    start += 1;
-                }
+    for k in key.chars() {
+        while Some(k) != chars.get(0).copied() {
+            if let Some(last_item) = chars.pop() {
+                chars.insert(0, last_item);
             }
-        } else {
-            if let Some(char) = chars.get(start) {
-                matrix[rows - n - 1][j] = *char;
-                start += 1;
-            }
+            ret += 1
         }
     }
 
-    for r in 0..rows {
-        for c in 0..num_columns {
-            if matrix[r][c] != '0' {
-                ret.push(matrix[r][c]);
-            }
-        }
-    }
-
-    ret.iter().collect::<String>()
+    ret
 }
