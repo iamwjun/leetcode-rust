4ab1270fcf199e7a32803e8032cfccc1e019db33
iamwjun <cddxwujun@hotmail.com>
2024-02-02 09:55:57 +0800

    docs: 2670. 找出不同元素数目差数组

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let nums = vec![1,2,1,2];
+use std::collections::HashSet;
 
-    println!("{:?}", minimum_seconds(nums));
+fn main() {
+    let nums = vec![3,2,3,4,2];
+
+    println!("{:?}", distinct_difference_array(nums));
 }
 
 fn distinct_difference_array(nums: Vec<i32>) -> Vec<i32> {
+    let len = nums.len();
+    let mut ret: Vec<i32> = vec![0;len];
 
+    for i in 0..len {
+        let left: HashSet<_> = nums[0..(i + 1)].iter().cloned().collect();
+        let right: HashSet<_> = nums[(i + 1)..len].iter().cloned().collect();
+        ret[i] = left.len() as i32 - right.len() as i32;
+    }
+
+    ret
 }
